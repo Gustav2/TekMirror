46f8795f21d516c397f205cfe2e4b2ab9115f355
DNIIBOY <32793938+DNIIBOY@users.noreply.github.com>
2021-12-24 03:05:42 +0100

    adding parse to module

--- modules/RejseplanenModule/RejseplanenModule.js
+++ modules/RejseplanenModule/RejseplanenModule.js
@@ -1,21 +1,79 @@
 let jsonData = "Hej";
-const {data} = require("grunt/lib/grunt/config");
+
 const request = require('sync-request');
+
+function getJSON(url){
+    res = request('GET', url);
+    stringBody = res.getBody('utf-8');
+    jsonBody = JSON.parse(stringBody);
+    return jsonBody;
+}
+
+function parseJSON(body, trainOnly){
+    if (trainOnly) {
+        body["TripList"]["Trip"][0]["Leg"].forEach(trip => {
+            if (trip.type == "REG") {
+                output = `Tog fra: ${trip["Origin"]["name"]}\n`;
+                output += `Til: ${trip["Destination"]["name"]}\n`;
+                output += `${trip["Notes"]["text"].split(";")[0]}\n`;
+                output += `Spor: ${trip["Origin"]["rtTrack"]}\n`;
+                if (trip.rtTime) {
+                    output += `Kl: <strike>${trip["Origin"]["time"]}</strike>\n`;
+                    output += `Kl: ${trip["Origin"]["rtTime"]}`;
+                }
+                else {
+                    output += `Kl: ${trip["Origin"]["time"]}\n`;
+                }
+                if (trip["Destination"]["rtTime"]) {
+                    output += `Ankomst: <strike>${trip["Destination"]["time"]}</strike>\n`;
+                    output += `Ankomst: ${trip["Destination"]["rtTime"]}\n`;
+                }
+                else {
+                    output += `Ankomst: ${trip["Destination"]["time"]}\n`;
+                }
+            }
+        });
+    }
+
+    if (!trainOnly) {
+        body["TripList"]["Trip"][0]["Leg"].forEach(trip => {
+            if (trip.type == "BUS") {
+                output = `Bus fra: ${trip["Origin"]["name"]}\n`
+                output += `Mod: ${trip["Destination"]["name"]}\n`
+                output += `${trip["Notes"]["text"].split(";")[0]}\n`
+                if (trip.rtTime) {
+                    output += `Kl: <strike>${trip["Origin"]["time"]}</strike>\n`
+                    output += `Kl: ${trip["Origin"]["rtTime"]}\n`
+                }
+                else {
+                    output += `Kl: ${trip["Origin"]["time"]}\n`
+                }
+                if (trip["Destination"]["rtTime"]) {
+                    output += `Ankomst: <strike>${trip["Destination"]["time"]}</strike>\n`
+                    output += `Ankomst: ${trip["Destination"]["rtTime"]}\n`
+                }
+                else {
+                    output += `Ankomst: ${trip["Destination"]["time"]}\n`
+                }
+            }
+        });
+    }
+    return output;
+}
 
 
 Module.register("RejseplanenModule",{
 	// Default module config.
 	defaults: {
 		text: jsonData,
-		jsonURL: "https://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originCoordX=9550145&originCoordY=56166726&originCoordName=College360&destCoordX=9783200&destCoordY=56082440&destCoordName=Hjem&useBus=0&format=json",
-		showTrains: true
+        url: "https://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originCoordX=9783200&originCoordY=56082440&originCoordName=Hjem&destCoordX=9550145&destCoordY=56166726&destCoordName=College360&useBus=0&format=json"
 	},
 
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = this.config.text;
-		// wrapper.innerHTML = "Tog fra: Ry St.\nMod: Silkeborg St.\nSpor: 1\nKl: 13:69";
+        wrapper.innerHTML = parseJSON(getJSON(this.config.url), true);
 		return wrapper;
 	}
 });
