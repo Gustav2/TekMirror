b2a1a8046665edd2bb354d5e7c9948dab8c7818e
DNIIBOY <daniel.nettelfield@gmail.com>
2021-12-24 02:42:52 +0100

    Now done synchronous, so it works and returns stuff

--- modules/RejseplanenModule/RejseplanParse.js
+++ modules/RejseplanenModule/RejseplanParse.js
@@ -4,48 +4,44 @@ let toRy = "https://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originCoordX=955014
 let fromSpice = "https://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originCoordX=9512079&originCoordY=56178240&originCoordName=Hjem&destCoordX=9550145&destCoordY=56166726&destCoordName=College360&useBus=1&format=json"
 let toSpice = "https://xmlopen.rejseplanen.dk/bin/rest.exe/trip?originCoordX=9550145&originCoordY=56166726&originCoordName=College360&destCoordX=9512079&destCoordY=56178240&destCoordName=Hjem&useBus=0&format=json"
 
-const { response } = require('express');
-const fs = require('fs');
 
-function getJSON(url, callback) {
-    const request = require('request');
-    let options = { json: true };
-    request(url, options, (error, res, body) => {
-        if (!error && res.statusCode == 200){
-            callback(body);
-        }
-        else{
-            console.log(error);
-        }
-    });
+const { from } = require('form-data');
+const { overArgs } = require('lodash');
+const request = require('sync-request');
+
+function getJSON(url){
+    res = request('GET', url);
+    stringBody = res.getBody('utf-8');
+    jsonBody = JSON.parse(stringBody);
+    return jsonBody;
 }
 
-function parseJSON(trainOnly){
+function parseJSON(body, trainOnly){
     if (trainOnly) {
-        console.log(body["TripList"]["Trip"][0]["Leg"])
         body["TripList"]["Trip"][0]["Leg"].forEach(trip => {
             if (trip.type == "REG") {
-                output = `Tog fra: ${trip["Origin"]["name"]}\n`
-                output += `Til: ${trip["Destination"]["name"]}\n`
-                output += `${trip["Notes"]["text"].split(";")[0]}\n`
-                output += `Spor: ${trip["Origin"]["rtTrack"]}\n`
+                output = `Tog fra: ${trip["Origin"]["name"]}\n`;
+                output += `Til: ${trip["Destination"]["name"]}\n`;
+                output += `${trip["Notes"]["text"].split(";")[0]}\n`;
+                output += `Spor: ${trip["Origin"]["rtTrack"]}\n`;
                 if (trip.rtTime) {
-                    output += `Kl: <strike>${trip["Origin"]["time"]}</strike>\n`
-                    output += `Kl: ${trip["Origin"]["rtTime"]}`
+                    output += `Kl: <strike>${trip["Origin"]["time"]}</strike>\n`;
+                    output += `Kl: ${trip["Origin"]["rtTime"]}`;
                 }
                 else {
-                    output += `Kl: ${trip["Origin"]["time"]}\n`
+                    output += `Kl: ${trip["Origin"]["time"]}\n`;
                 }
                 if (trip["Destination"]["rtTime"]) {
-                    output += `Ankomst: <strike>${trip["Destination"]["time"]}</strike>\n`
-                    output += `Ankomst: ${trip["Destination"]["rtTime"]}\n`
+                    output += `Ankomst: <strike>${trip["Destination"]["time"]}</strike>\n`;
+                    output += `Ankomst: ${trip["Destination"]["rtTime"]}\n`;
                 }
                 else {
-                    output += `Ankomst: ${trip["Destination"]["time"]}\n`
+                    output += `Ankomst: ${trip["Destination"]["time"]}\n`;
                 }
             }
         });
     }
+
     if (!trainOnly) {
         body["TripList"]["Trip"][0]["Leg"].forEach(trip => {
             if (trip.type == "BUS") {
@@ -69,13 +65,12 @@ function parseJSON(trainOnly){
             }
         });
     }
+    return output;
 }
 
-getJson(fromRy, function(data) {
-    console.log(data)
-});
-
-console.log(getJSON(fromRy));
+bod = getJSON(fromRy);
+output = parseJSON(bod, true);
+console.log(output);
 
 //var out = rejseplan(toRy, true);
 //console.log(out);
